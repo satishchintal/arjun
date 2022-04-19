33e3546b788c2b999ebea58bad93bbd956905488
satishchintal <97095294+satishchintal@users.noreply.github.com>
2022-04-20 02:38:58 +0530

    Add files via upload

---
+++ ramesh/src/main/java/ramesh/ramesh/RameshApplication.java
@@ -0,0 +1,13 @@
+package ramesh.ramesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RameshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RameshApplication.class, args);
+	}
+
+}
---
+++ ramesh/src/test/java/ramesh/ramesh/RameshApplicationTests.java
@@ -0,0 +1,13 @@
+package ramesh.ramesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RameshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
